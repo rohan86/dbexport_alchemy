1a67c68fca5a80c78f312b4160612053b91e2e01
rt1601 <rohan.thareja@gmail.com>
2020-05-07 21:16:32 +0800

    initial commit

--- src/config.py
+++ src/config.py
@@ -1,8 +1,28 @@
 import os
-import functools
-# from sqlalchemy import create_engine
+from functools import lru_cache
 
-def funct():
-   print ("Hello")
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-funct()
+
+@lru_cache(maxsize=32)
+def engine(db_url=None):
+    db_url = db_url or os.getenv("DB_URL")
+    if not db_url:
+        raise ValueError("database URL is required")
+    return create_engine(db_url)
+
+
+def get_connection(db_url=None):
+    return engine(db_url).connect()
+
+
+@lru_cache(maxsize=32)
+def session_class(db_url=None):
+    return sessionmaker(bind=engine(db_url))
+
+
+try:
+    Session = session_class()
+except:
+    pass
